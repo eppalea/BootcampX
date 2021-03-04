97a5062a96d93fd4885f38899bd1a2f4ab55787f
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-04 06:30:57 +0000

    change to a parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,39 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as id, students.name as name, cohorts.name as cohort_id
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
-  })
-})
-.catch(err => console.log('query error', err.stack));
+// pool.query(`
+// SELECT students.id as id, students.name as name, cohorts.name as cohort_id
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+//   })
+// })
+// .catch(err => console.log('query error', err.stack));
+
+
+//Parameterized Query
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+  
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values, (err, res) => {
+  if (err) {
+    console.log(err.stack)
+  } else {
+    console.log(res.rows[0])
+  }
+});
