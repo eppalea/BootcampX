a8bf0e7e40d4757af77ac5eda16f1f56ab7977b6
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-04 04:37:36 +0000

    add JOIN to query to obtain the cohort name

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,11 +8,14 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT id, name, cohort_id
+SELECT students.id as id, students.name as name, cohorts.name as cohort_id
 FROM students
+JOIN cohorts ON cohorts.id = cohort_id
 LIMIT 5;
 `)
 .then(res => {
-  console.log(res.rows);
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+  })
 })
 .catch(err => console.log('query error', err.stack));
